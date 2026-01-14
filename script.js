1c6e5f0c5388e1f8410e8d7d8682da82b2431916
bannna2278 <lpbogart2011@gmail.com>
2026-01-13 20:31:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ btn.addEventListener('click', () => {
   // 3. Toggle the class on the body
   document.body.classList.toggle('dark-mode');
 });
+
+const ctaBtn = document.getElementById('call-to-action');
+
+ctaBtn.addEventListener('click', () => {
+    window.location.href = 'https://forms.gle/oAMhnqiYG447XmCk9';});
